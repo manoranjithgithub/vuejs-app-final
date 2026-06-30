8f4d2a6579b6d7a46585be03ebc9870a9649c89a
manoranjith <manoranjith@dilligentech.com>
2026-06-30 18:41:08 +0530

    Updates Text

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your premium Pro Vue.js App 1"/>
+  <HelloWorld msg="Premium Pro Vue.js App 1"/>
 </template>
 
 <script>
